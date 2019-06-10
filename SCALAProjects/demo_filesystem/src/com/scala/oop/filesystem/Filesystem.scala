50a47f266524b5b88fb538a5512a0c34425aae4c
bestils <thebestilspl@gmail.com>
2019-06-10 19:34:14 +0200

    Initial commit, added $ sign and infinity loop

---
+++ SCALAProjects/demo_filesystem/src/com/scala/oop/filesystem/Filesystem.scala
@@ -0,0 +1,15 @@
+package com.scala.oop.filesystem
+
+import java.util.Scanner
+
+
+object Filesystem extends  App{
+
+  val sanner = new Scanner(System.in)
+
+  while (true){
+    print("$")
+    println(sanner.next)
+  }
+
+}

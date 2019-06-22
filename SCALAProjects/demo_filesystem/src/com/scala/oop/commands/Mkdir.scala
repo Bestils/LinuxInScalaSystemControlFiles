e5dd8b4560001435ec10dd08f5dafc510c622521
bestils <thebestilspl@gmail.com>
2019-06-22 13:28:47 +0200

    mk dir maded

--- SCALAProjects/demo_filesystem/src/com/scala/oop/commands/Mkdir.scala
+++ SCALAProjects/demo_filesystem/src/com/scala/oop/commands/Mkdir.scala
@@ -1,5 +1,5 @@
 package com.scala.oop.commands
-import com.scala.oop.files.Directory
+import com.scala.oop.files.{DirEntry, Directory}
 import com.scala.oop.filesystem.State
 
 class Mkdir(name:String) extends Command {
@@ -10,9 +10,6 @@ class Mkdir(name:String) extends Command {
 
 
 
-  def doMkdir(state: State, name: String): State = {
-    ???
-  }
 
   override def apply(state: State): State = {
     val wd = state.wd
@@ -20,7 +17,7 @@ class Mkdir(name:String) extends Command {
       state.setMessage("Entry "+name+" already exists!")
     }
     else if (name.contains(Directory.SEPARATOR)){
-      //mkdir some
+     ???
     }
     else  if  ( checkIllegal(name)){
       state.setMessage(name+ " : illegal entry name!")
@@ -30,4 +27,31 @@ class Mkdir(name:String) extends Command {
     }
   }
 
+  def doMkdir(state: State, name: String): State = {
+  def updateStructure(currentDirectory: Directory, path: List[String], newEntry: DirEntry) : Directory = {
+    if(path.isEmpty) currentDirectory.addEntry(newEntry)
+    else {
+      val oldEntry = currentDirectory.findEntry(path.head).asDirectory
+      currentDirectory.replaceEntry(oldEntry.name ,updateStructure(oldEntry,path.tail,newEntry))
+      // currentDirectory = /a
+      // path =["b"]
+
+    }
+  }
+
+
+    val  wd = state.wd
+
+    // 1. all the directories in the full path
+  val allDirsInPath = wd.getAllFoldersInPath
+
+    // 2. create new directory entry in the wd
+  val newDir = Directory.empty(wd.path,name)
+    // 3. update the whole directory structure starting from the rooot ( the directory structure is IMMUTABLE )
+    val newRoot = updateStructure(state.root,allDirsInPath,newDir)
+
+    // 4. find new working directory INSTANCE given wd's full path, in the NEW directory structure
+    val newWd = newRoot.findDescendant(allDirsInPath)
+
+  }
 }
--- SCALAProjects/demo_filesystem/src/com/scala/oop/files/Directory.scala
+++ SCALAProjects/demo_filesystem/src/com/scala/oop/files/Directory.scala
@@ -1,7 +1,22 @@
 package com.scala.oop.files
 
-class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry]) extends DirEntry(parentPath , name) {
+class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
+  extends DirEntry(parentPath , name) {
+  def replaceEntry(entryName: Any, newEntry: DirEntry):Directory  = ???
+
+  def findEntry(entryName: String) = ???
+
+  def addEntry(newEntry: DirEntry) = ???
+
   def hasEntry(name: String): Boolean = ???
+
+  def getAllFoldersInPath : List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList
+
+  def findDescendant(path: List[String]):Directory = ???
+
+
+  def asDirectory : Directory = this
 
 }
 object Directory{
